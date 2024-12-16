7a91cda86578d82d78fb94bdce44cf3a4906f4d9
nutorbit <nutorbitx@gmail.com>
2024-12-16 15:59:43 +0000

    update config & use dora

--- datax_flowertune/models.py
+++ datax_flowertune/models.py
@@ -56,6 +56,7 @@ def get_model(model_cfg: DictConfig):
         lora_alpha=model_cfg.lora.peft_lora_alpha,
         lora_dropout=0.075,
         task_type="CAUSAL_LM",
+        use_dora=True,
     )
 
     if model_cfg.gradient_checkpointing:
